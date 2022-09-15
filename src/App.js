a3983d7609a4806d4a7faca150484f2cd214e3c8
mary <anacoin0014@gmail.com>
2022-09-15 19:30:49 +0430

    add todo with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ToDo from './component/Todo/ToDo';
+// import 
+// const users =[{id:1,name:'maryam',lastname:'kohan'},
+// {id:2,name:'maryam',lastname:'dds'},
+// {id:3,name:'maryam',lastname:'ffdd'},
+// ];
+
+const todos =[{
+  id:1,
+  title:"title",
+  description:"Say",
+  state:false
+},
+{
+  id:2,
+  title:"new",
+  description:"Say",
+  state:false},
+{
+  id:3,
+  title:"buy",
+  description:"Say",
+  state:true}
+];
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [state, setstate] = useState(todos);
+  const additem =(e)=>{
+  e.preventDefault()
+  const formdata = new FormData(e.target);
+  const data =Object.fromEntries(formdata.entries());
+  setstate([...state,{...data,id:Math.random()*100}]);
+}
+  return(
+    <div>
+      <form onSubmit={additem}>
+        <lable>name</lable>
+        <input type="text" name='firstname'></input>
+        <lable>lastname</lable>
+        <input type="text" name='lastname'></input>
+        
+        <button>add</button>
+      </form>
+      {state.map((todo)=>(
+        <ToDo id={todo.id.toString()}
+         title={todo.title}
+         description={todo.description}/>
+      )
+      )}
     </div>
-  );
+  )
+  
 }
 
 export default App;
+
+
---
+++ src/component/Todo/ToDo.jsx
@@ -0,0 +1,12 @@
+
+const ToDo =({id,title,description}) =>{
+    return(
+        <div>
+            <h1>{id}</h1>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+export default ToDo 
